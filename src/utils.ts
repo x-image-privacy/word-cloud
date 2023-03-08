ee7297d5439ef7f6d3a5ab94c5a5647e7a2f8ac8
RoxaneBurri <roxane.burri@etu.unige.ch>
2023-03-08 12:51:20 +0100

    feat: next position of word and collision

--- src/utils.ts
+++ src/utils.ts
@@ -142,11 +142,128 @@ export const netForce = (
   return [sumDiffX, sumDiffY];
 };
 
-export const distance = (
+export const getTriangleFromNetForce = (
+  word: {
+    id: string;
+    text: string;
+    x: number;
+    y: number;
+  },
+  netForce: [number, number]
+) => {
+  const lenghtHypontenus = Math.sqrt(
+    Math.pow(netForce[0] - word.x, 2) + Math.pow(netForce[1] - word.y, 2)
+  );
+
+  const deltaX =
+    ((netForce[1] - word.y) * lenghtHypontenus) / (2 * lenghtHypontenus);
+  const deltaY =
+    ((netForce[0] - word.x) * lenghtHypontenus) / (2 * lenghtHypontenus);
+
+  const midPointX = (word.x + netForce[0]) / 2;
+  const midPointY = (word.y + netForce[1]) / 2;
+
+  const rightTriangleVertexX = midPointX + deltaX / 2;
+  const rightTriangleVertexY = midPointY + deltaY / 2;
+
+  return [rightTriangleVertexX, rightTriangleVertexY];
+};
+
+export const moveOnHypotenus = (
+  A: [x: number, y: number], // adjacent - hypotenuse
+  B: [x: number, y: number], // hypotenuse - opposite
+  C: [x: number, y: number], // right angle
+  step: number
+) => {
+  const lenghtHypontenus = distanceBetweenPoint(A, B);
+
+  const distanceCA =
+    ((A[0] - C[0]) * (B[0] - A[0]) + (A[1] - C[1]) * (B[1] - A[1])) /
+    lenghtHypontenus;
+
+  const newX = A[0] + (step * (B[0] - A[0])) / lenghtHypontenus;
+  const newY = A[1] + (step * (B[1] - A[1])) / lenghtHypontenus;
+
+  return [newX, newY];
+};
+
+export const futurPosition = (
+  word: {
+    id: string;
+    text: string;
+    x: number;
+    y: number;
+  },
+  passRect: {
+    id: string;
+    text: string;
+    x: number;
+    y: number;
+  }[],
+  step: number,
+  w1H: number,
+  w1W: number,
+  w2H: number,
+  w2W: number
+) => {
+  let isCollision = 1;
+  let netForceW;
+  let rightAnglePoint;
+
+  let tmpWPosition = [word.x, word.y];
+  let tmpW = word;
+
+  while (isCollision) {
+    netForceW = netForce(word, passRect);
+
+    rightAnglePoint = getTriangleFromNetForce(word, netForceW);
+
+    tmpWPosition = moveOnHypotenus(
+      tmpWPosition,
+      netForceW,
+      rightAnglePoint,
+      step
+    );
+
+    tmpW.x = tmpWPosition[0];
+    tmpW.y = tmpWPosition[1];
+
+    isCollision = allCollision(word, passRect);
+  }
+};
+
+export const distanceBetweenWord = (
   w1: { id: string; text: string; x: number; y: number },
   w2: { id: string; text: string; x: number; y: number }
 ) => {
   return Math.sqrt(Math.pow(w2.x - w1.x, 2) + Math.pow(w2.y - w1.y, 2));
+};
+
+export const distanceBetweenPoint = (
+  w1: [x: number, y: number],
+  w2: [x: number, y: number]
+) => {
+  return Math.sqrt(Math.pow(w2[0] - w1[0], 2) + Math.pow(w2[1] - w1[1], 2));
+};
+
+export const collision = (
+  w1: { id: string; text: string; x: number; y: number },
+  w2: { id: string; text: string; x: number; y: number }
+) => {
+  const positionW1 = getCoordinateOfParent(w1.id);
+  const positionW2 = getCoordinateOfParent(w2.id);
+  if (
+    w1.x < w2.x + positionW2[2] &&
+    w1.x + positionW1[2] > w2.x &&
+    w1.y < w2.y + positionW2[3] &&
+    positionW1[3] + w1.y > w2.y
+  ) {
+    console.log("collision");
+    return 1;
+  } else {
+    console.log("No collision");
+    return 0;
+  }
 };
 
 export const allCollision = (
@@ -154,38 +271,16 @@ export const allCollision = (
   wordArray: any[]
 ): any => {
   wordArray.forEach((w: { id: string; text: string; x: number; y: number }) => {
-    if (word.id != w.id) {
+    if (word.id !== w.id) {
       if (collision(word, w)) {
         console.log("collision");
-        return true;
+        return 1;
       } else {
         console.log("No collision");
 
-        return false;
+        return 0;
       }
     }
-    return false;
+    return 0;
   });
-};
-
-export const collision = (
-  w1: { id: string; text: string; x: number; y: number },
-  w2: { id: string; text: string; x: number; y: number },
-  w1H: number,
-  w1W: number,
-  w2H: number,
-  w2W: number
-) => {
-  if (
-    w1.x < w2.x + w2W &&
-    w1.x + w1W > w2.x &&
-    w1.y < w2.y + w2H &&
-    w1H + w1.y > w2.y
-  ) {
-    console.log("collision");
-    return true;
-  } else {
-    console.log("No collision");
-    return false;
-  }
 };
